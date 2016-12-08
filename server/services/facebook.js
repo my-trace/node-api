9a9850743fc6dffd4e820023b3f82814dbae49dc
Andrew Carlson <acarl005@g.ucla.edu>
2016-12-08 02:40:38 -0800

    add caching for facebook tokens

--- server/services/facebook.js
+++ server/services/facebook.js
@@ -1,24 +1,36 @@
 const env = require('../../env')
+const Cache = require('node-cache')
 
+// use a simple process-memory cache for facebook tokens
+const keyCache = new Cache({ stdTTL: 3600 }) // on hour
 const User = require('../models/user')
 
-// ask facebook if the token they gave us is valid
+// ask facebook (unless cached) if the token they gave us is valid
 exports.auth = function* (next) {
   const token = this.get('Authorization')
+  // without a token, they are definitely not authorized
   if (!token) {
     this.status = 401
     return this.body = 'unauthorized'
   }
-  const request = this.app.context.request
-  const options = {
-    url: 'https://graph.facebook.com/debug_token?',
-    json: true,
-    qs: {
-      input_token: token,
-      access_token: env.FB_APP_KEY + '|' + env.FB_APP_SECRET
+  // see if this token has been checked in the past hour
+  let reply = keyCache.get(token)
+  // if not, ask facebook
+  if (!reply) {
+    console.log('talking to FB')
+    const request = this.app.context.request
+    const options = {
+      url: 'https://graph.facebook.com/debug_token?',
+      json: true,
+      qs: {
+        input_token: token,
+        access_token: env.FB_APP_KEY + '|' + env.FB_APP_SECRET
+      }
     }
+    reply = yield request(options)
+    // and save it to the cache
+    keyCache.set(token, reply)
   }
-  const reply = yield request(options)
   if (reply.data.is_valid) {
     const knex = this.app.context.db
     this.facebookId = parseInt(reply.data.user_id)
