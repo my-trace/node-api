dc4740d7e9c80463d2d7cd4b50a5cbb3fedc7405
Alex Zai <azai91@gmail.com>
2016-12-09 03:05:57 -0800

    remove defaults from env.js

--- env.js
+++ env.js
@@ -3,10 +3,10 @@ const { num, url, str } = envalid
 
 const env = envalid.cleanEnv(process.env, {
   PORT: num({ default: 3333 }),
-  PG_URL: url({ default: 'postgres://localhost/trace-dev'}),
-  FB_APP_KEY: str({ default: '364038477099861'}),
-  FB_APP_SECRET: str({ default: 'e17b94dd981ece557bae550fc0a9a7cb'}),
-  HOST: str({ default: 'localhost:3333'}),
+  PG_URL: url(),
+  FB_APP_KEY: str(),
+  FB_APP_SECRET: str(),
+  HOST: str(),
 })
 
 module.exports = env
